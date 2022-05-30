4bb9ec1fae784632a91e1ba551b204f9eba13afa
micschwarz <contact@micschwarz.dev>
2022-05-30 21:25:00 +0200

    Improve controls responsiveness

--- src/lib/message/MessageInput.svelte
+++ src/lib/message/MessageInput.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import Icon from '$lib/components/Icon.svelte';
+
     import { add } from '$lib/firebase/firestore/collections/messages';
     import { currentUser } from '$lib/user/userStore';
+    import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
     $: user = $currentUser;
 
@@ -42,11 +45,16 @@
             disabled={sending}
         />
     </div>
-    <div class="w-32">
+    <div class="w-12 sm:w-32">
         {#if sending}
-            <button class="btn btn-primary w-full loading" type="button" disabled>Sende</button>
+            <button class="btn btn-primary loading sm:w-full" type="button" disabled>
+                <span class="hidden sm:block">Sende</span>
+            </button>
         {:else}
-            <button class="btn btn-primary w-full" type="submit" disabled={!messageSendable}>Senden</button>
+            <button class="btn btn-primary sm:w-full" type="submit" disabled={!messageSendable}>
+                <Icon icon={faPaperPlane} />
+                <span class="ml-2 hidden sm:block">Senden</span>
+            </button>
         {/if}
     </div>
 </form>
