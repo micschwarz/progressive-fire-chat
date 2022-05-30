1d6411f68516bfffe454e346009be2bc31c5e20a
micschwarz <contact@micschwarz.dev>
2022-05-30 21:09:29 +0200

    Improve navbar responsiveness

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,10 +8,15 @@
 <div class="p-3">
     <div class="navbar bg-base-200 rounded-box">
         <div class="flex-1">
-            <a href="/" class="btn btn-ghost normal-case text-xl">Progressive Fire Chat 🔥</a>
+            <div class="hidden sm:block">
+                <a href="/" class="btn btn-ghost normal-case text-xl">Progressive Fire Chat 🔥</a>
+            </div>
+            <div class="block sm:hidden">
+                <a href="/" class="btn btn-square btn-ghost normal-case text-xl">🔥</a>
+            </div>
         </div>
         <div class="flex items-center gap-1">
-            <div class="mr-2">Hi {$currentUser?.name}</div>
+            <div class="mr-2 hidden sm:block">Hi {$currentUser?.name}</div>
             <a class="btn btn-ghost btn-square" href="/settings"><Icon icon={faCog} /></a>
             <button class="btn btn-ghost" on:click={logout}><Icon icon={faSignOut} /></button>
         </div>
