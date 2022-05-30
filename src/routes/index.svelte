02f18561d04da9c602f03bd4e7a78dd8ceffbae3
micschwarz <contact@micschwarz.dev>
2022-05-30 16:10:33 +0200

    Add daisyui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,1 @@
-<script lang="ts">
-    import { app } from '$lib/firebase';
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+Progressive Fire Chat 🔥
