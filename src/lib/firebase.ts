2353ada8d762086a5132d70b534e2175130f1553
micschwarz <contact@micschwarz.dev>
2022-05-30 16:00:39 +0200

    Fix firebase stuff

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,19 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { connectAuthEmulator, getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_API_ID,
+    apiKey: 'AIzaSyCqfsuXybFZNKbaef0IbiuOmmx1Vh2TJDU',
+    authDomain: 'progressive-fire-chat.firebaseapp.com',
+    projectId: 'progressive-fire-chat',
+    storageBucket: 'progressive-fire-chat.appspot.com',
+    messagingSenderId: '432825278201',
+    appId: '1:432825278201:web:00127bc2ebba12eb8b8c28',
 };
 
 export const app = initializeApp(firebaseConfig);
 
 export let auth = getAuth(app);
 if (import.meta.env.DEV) {
-    console.log('use auth emulator');
-
     connectAuthEmulator(auth, 'http://localhost:9099');
 }
